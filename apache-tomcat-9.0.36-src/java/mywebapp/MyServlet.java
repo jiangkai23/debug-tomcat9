36079d72c6b347f1ebfbb087b5d77a40cd6be526
jiangkai <jiangkai_23@163.com>
2020-07-15 21:25:05 +0800

    author

--- apache-tomcat-9.0.36-src/java/mywebapp/MyServlet.java
+++ apache-tomcat-9.0.36-src/java/mywebapp/MyServlet.java
@@ -9,15 +9,15 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 /**
- * @author: XiaMu
- * @create: 2020-07-09 14:23
+ * @author XiaMu
+ * @date 2020-07-09 14:23
  **/
 @WebServlet("/myServlet")
 public class MyServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        PrintWriter pw= resp.getWriter();
+        PrintWriter pw = resp.getWriter();
         pw.println("hello MyServlet");
     }
 
